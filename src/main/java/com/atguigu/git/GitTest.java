2f53686b02ace749f4379f085e2cf0a1c88d761b
Sleepy <sleepy20211111@gmail.com>
2023-02-06 13:31:34 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atguigu.git;
+
+/**
+ * @author Sleepy
+ * @create 2023-02-06 13:23
+ */
+public class GitTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("git test");
+    }
+}
