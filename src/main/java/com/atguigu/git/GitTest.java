607128d70e4cd7c87bf81cc60374ebd30d7b21dc
Sleepy20211111 <96278573+Sleepy20211111@users.noreply.github.com>
2023-02-06 15:39:35 +0800

    pull new

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -16,6 +16,6 @@ public class GitTest
         System.out.println("hot-fix test");
         System.out.println("push test");
         System.out.println("push test2");
-        System.out.println("pull test newnew");
+        System.out.println("pull new version");
     }
 }
